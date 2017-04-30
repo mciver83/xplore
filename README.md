47eacb8fd8c547e5a8b1e30aa9d05254c98f6259
Mark McIver <mark@objectiveinc.com>
2017-04-30 15:43:40 -0600

    lint

---
+++ README.md
@@ -0,0 +1,31 @@
+Xplore
+--------
+
+## Commands
+
+```sh
+# Install dependencies
+$ yarn
+
+# Start the JS packager;
+# IMPORTANT: This must be running when using dev mode.
+$ yarn start
+
+# Run the project on an iOS simulator
+$ react-native run-ios
+
+# Run the project on an Android simulator
+$ react-native run-android
+
+# Run specs
+$ yarn test
+
+# Run specs continuously (on file change)
+$ yarn test:watch
+
+# Run the linter
+$ yarn lint
+
+# Run the linter, fix minor errors automatically
+$ yarn lint:fix
+```
--- app/containers/RouteContainer.js
+++ app/containers/RouteContainer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { NativeRouter, Route, Switch } from 'react-router-native';
 
-import { authenticate, LoginContainer } from './auth';
+import { LoginContainer } from './auth';
 import AppContainer from './AppContainer';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
 import Dashboard from '../components/Dashboard';
-// import TripsRoutesContainer from './trips/TripsRoutesContainer';
-
 
 const RouteContainer = () => (
   <NativeRouter>
@@ -16,7 +14,7 @@ const RouteContainer = () => (
         <Route path="/" exact component={Home} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/profile" component={Profile} />
-			
+
         <Route path="/auth/login" component={LoginContainer} />
       </Switch>
     </AppContainer>
